3440eed8e84d91b771e1c097d23ea82197fc790d
jmnauman <jnauman6001899@gmail.com>
2025-02-12 10:24:30 -0800

    316 - Add a new function to firebolt for spawning multiple projectiles in a spread. For now, just draws debug arrows.

--- Source/Aura/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
@@ -3,6 +3,8 @@
 
 #include "AbilitySystem/Abilities/AuraFireBolt.h"
 
+#include "Kismet/KismetSystemLibrary.h"
+
 FString UAuraFireBolt::GetDescription(int32 Level)
 {
 	const int32 ScaledDamage = Damage.GetValueAtLevel(Level);
@@ -94,4 +96,48 @@ FString UAuraFireBolt::GetNextLevelDescription(int32 Level)
 			Cooldown,
 			FMath::Min(Level, NumProjectiles),
 			ScaledDamage);
+}
+
+void UAuraFireBolt::SpawnProjectiles(
+	const FVector& ProjectileTargetLocation,
+	const FGameplayTag& SocketTag,
+	AActor* HomingTarget,
+	bool bOverridePitch,
+	float PitchOverride)
+{
+	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
+	if (!bIsServer) return;
+
+	const FVector SocketLocation = ICombatInterface::Execute_GetCombatSocketLocation(
+		GetAvatarActorFromActorInfo(),
+		SocketTag);
+	FRotator Rotation = (ProjectileTargetLocation - SocketLocation).Rotation();
+	if (bOverridePitch)
+	{
+		Rotation.Pitch = PitchOverride;
+	}
+
+	const FVector Forward = Rotation.Vector();
+	const FVector LeftOfSpread = Forward.RotateAngleAxis(-0.5f * ProjectileSpread, FVector::UpVector);
+	const FVector RightOfSpread = Forward.RotateAngleAxis(0.5f * ProjectileSpread, FVector::UpVector); // Not needed, just for debug
+	
+	//NumProjectiles = FMath::Min(MaxNumProjectiles, GetAbilityLevel());
+	if (NumProjectiles > 1)
+	{
+		const float DeltaSpread = ProjectileSpread / (NumProjectiles - 1); 
+
+		for (int32 i = 0; i < NumProjectiles; i++)
+		{
+			const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);
+			UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), SocketLocation, SocketLocation + Direction * 75.f, 5.f, FColor::Yellow, 120.f, 2.f);
+		}
+	}
+	else
+	{
+		// todo single projectile
+	}
+	
+	UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), SocketLocation, SocketLocation + Forward * 100.f, 5.f, FColor::Green, 120.f, 2.f);
+	UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), SocketLocation, SocketLocation + LeftOfSpread * 100.f, 5.f, FColor::Red, 120.f, 2.f);
+	UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), SocketLocation, SocketLocation + RightOfSpread * 100.f, 5.f, FColor::Purple, 120.f, 2.f);
 }
--- Source/Aura/Public/AbilitySystem/Abilities/AuraFireBolt.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraFireBolt.h
@@ -7,7 +7,8 @@
 #include "AuraFireBolt.generated.h"
 
 /**
- * 
+ * Class representing a firebolt spell. Unlike the base projectile, this class can spawn multiple projectiles
+ * that home in on a target
  */
 UCLASS()
 class AURA_API UAuraFireBolt : public UAuraProjectileSpell
@@ -16,4 +17,19 @@ class AURA_API UAuraFireBolt : public UAuraProjectileSpell
 public:
 	virtual FString GetDescription(int32 Level) override;
 	virtual FString GetNextLevelDescription(int32 Level) override;
+
+	UFUNCTION(BlueprintCallable)
+	void SpawnProjectiles(
+		const FVector& ProjectileTargetLocation,
+		const FGameplayTag& SocketTag,
+		AActor* HomingTarget,
+		bool bOverridePitch = false,
+		float PitchOverride = 0.f);
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Firebolt")
+	float ProjectileSpread = 90.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Firebolt")
+	int32 MaxNumProjectiles = 5;
 };
