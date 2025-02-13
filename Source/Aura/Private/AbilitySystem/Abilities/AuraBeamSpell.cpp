1a25f4099358f7d93b9f89ba7ebaa13b6f24ef6c
jmnauman <jnauman6001899@gmail.com>
2025-02-13 15:16:26 -0800

    329 - Use a sphere trace to end the schok beam at the first CombatInterface that was hit, if applicable. If the target is a CombatInterface, attach the beam cue to that actor instead of the player. This will facilitate chaining extra beams between enemies in a future lecture

--- Source/Aura/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
@@ -4,6 +4,7 @@
 #include "AbilitySystem/Abilities/AuraBeamSpell.h"
 
 #include "GameFramework/Character.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 void UAuraBeamSpell::StoreMouseDataInfo(const FHitResult& HitResult, bool& bMouseDataWasStored)
 {
@@ -26,5 +27,42 @@ void UAuraBeamSpell::StoreOwnerVariables()
 	{
 		OwnerPlayerController = CurrentActorInfo->PlayerController.Get();
 		OwnerCharacter = Cast<ACharacter>(CurrentActorInfo->AvatarActor);
+	}
+}
+
+void UAuraBeamSpell::TraceFirstTarget(const FVector& BeamTargetLocation)
+{
+	check(OwnerCharacter);
+
+	if (OwnerCharacter->Implements<UCombatInterface>())
+	{
+		if (USkeletalMeshComponent* Weapon = ICombatInterface::Execute_GetWeapon(OwnerCharacter))
+		{
+			const FVector SocketLocation = Weapon->GetSocketLocation(FName("TipSocket"));
+			
+			TArray<AActor*> ActorsToIgnore;
+			ActorsToIgnore.Add(OwnerCharacter);
+
+			FHitResult HitResult;
+
+			// We use a sphere trace to make this a little more "spongy" than a line trace
+			UKismetSystemLibrary::SphereTraceSingle(
+				OwnerCharacter,
+				SocketLocation,
+				BeamTargetLocation,
+				10.f,
+				TraceTypeQuery1,
+				false,
+				ActorsToIgnore,
+				EDrawDebugTrace::None,  //EDrawDebugTrace::ForDuration, //Use this to visualize
+				HitResult,
+				true);
+
+			if (HitResult.bBlockingHit)
+			{
+				MouseHitLocation = HitResult.ImpactPoint;
+				MouseHitActor = HitResult.GetActor();
+			}
+		}
 	}
 }
